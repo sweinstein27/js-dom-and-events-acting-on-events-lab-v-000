59894b6293c9d44c888008ce7f4ebbf75af83339
Scott Weinstein <sweinstein27@gmail.com>
2018-10-31 13:42:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,4 +29,11 @@ function addNewLiOnClick(){
 function clearEmployeeListOnLinkClick(){
   let link = document.querySelector('a')
   let ul = document.querySelector('ul')
+  link.addEventListener('click', function(event){
+    ul.innerHTML = ''
+  })
+}
+
+function resetInput(){
+  document.querySelector('input').value = ''
 }
