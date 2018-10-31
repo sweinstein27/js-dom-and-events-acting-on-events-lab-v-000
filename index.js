d37c2492d906d75ff7f1eda5ad26f99890495bf3
Scott Weinstein <sweinstein27@gmail.com>
2018-10-31 13:38:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ let input = document.querySelector('input')
 function retrieveEmployeeInformation(){
   return input.value
 }
+
+function addNewElementAsLi(){
+  let employeeName = retrieveEmployeeInformation()
+
+  document.querySelector('employee-list').insertAdjacentHTML('beforeend', `<li>${employeeName}</li>`)
+}
